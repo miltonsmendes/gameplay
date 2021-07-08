5ad140bb8ae78435e806e261999e1d4421eb6226
miltonsmendes <miltonsergiom@gmail.com>
2021-07-08 16:19:51 -0300

    daily update (Details screen)

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { createStackNavigator } from '@react-navigation/stack';
 
+import { theme } from "../global/styles/theme";
+
 import { Home } from "../screens/Home";
 import { SignIn } from "../screens/SignIn";
+import { AppointmentDetails } from "../screens/AppointmentDetails";
 
 const { Navigator, Screen } = createStackNavigator();
 
-export function AuthRoutes(){
+export function AuthRoutes() {
     return (
         <Navigator
             headerMode="none"
             screenOptions={{
                 cardStyle: {
-                    backgroundColor: 'transparent',
+                    backgroundColor: theme.colors.secondary100
                 }
             }}
         >
@@ -24,6 +27,10 @@ export function AuthRoutes(){
                 name="Home"
                 component={Home}
             />
+            <Screen
+                name="AppointmentDetails"
+                component={AppointmentDetails}
+            />
         </Navigator>
     );
- }
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, FlatList, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { CategorySelect } from "../../components/CategorySelect";
 import { ButtonAdd } from "../../components/ButtonAdd";
@@ -7,11 +8,15 @@ import { Profile } from "../../components/Profile";
 import { ListHeader } from "../../components/ListHeader";
 import { Appointment } from "../../components/Appointment";
 import { ListDivider } from "../../components/ListDivider";
+import { Background } from '../../components/Background';
 
 import { styles } from "./styles";
 
+
 export function Home() {
     const [category, setCategory] = useState('');
+
+    const navigation = useNavigation();
 
     const appointments = [
         {
@@ -44,8 +49,12 @@ export function Home() {
         categoryId === category ? setCategory('') : setCategory(categoryId);
     }
 
+    function handleAppointmentDetails() {
+        navigation.navigate('AppointmentDetails');
+    }
+
     return (
-        <View>
+        <Background>
             <View style={styles.header}>
                 <Profile />
                 <ButtonAdd />
@@ -66,13 +75,16 @@ export function Home() {
                     data={appointments}
                     keyExtractor={item => item.id}
                     renderItem={({ item }) => (
-                        <Appointment data={item} />
+                        <Appointment
+                            data={item}
+                            onPress={handleAppointmentDetails}
+                        />
                     )}
                     ItemSeparatorComponent={() => <ListDivider />}
                     style={styles.matches}
                     showsVerticalScrollIndicator={false}
                 />
             </View>
-        </View>
+        </Background>
     );
 }
