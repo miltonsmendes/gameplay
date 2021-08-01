2f17f2764fec6617ebf7ac17caae93b9e5a82078
miltonsmendes <miltonsergiom@gmail.com>
2021-08-01 03:37:10 -0300

    authentication feature update

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-49c8d0a5-8479-4b31-9277-b2ffe57f4f36';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '871236983064764476';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+    REDIRECT_URI,
+    SCOPE,
+    RESPONSE_TYPE,
+    CLIENT_ID,
+    CDN_IMAGE
+};
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,23 +3,30 @@ import {
     View,
     Text,
     Image,
+    Alert,
+    ActivityIndicator
 } from 'react-native';
-import { useNavigation } from "@react-navigation/native";
 
+import { useAuth } from '../../hooks/auth';
 
 import IllustrationImg from '../../assets/illustration.png';
+import { theme } from "../../global/styles/theme";
 import { styles } from './styles';
 
 import { ButtonIcon } from "../../components/ButtonIcon";
 import { Background } from '../../components/Background';
 
-
 export function SignIn() {
-    const navigation = useNavigation();
+    const { loading, signIn } = useAuth();
 
-    function handleSignIn() {
-        navigation.navigate('Home');
+    async function handleSignIn() {
+        try {
+            await signIn();
+        } catch (error) {
+            Alert.alert(error);
+        };
     }
+
     return (
         <Background>
             <View style={styles.container}>
@@ -41,10 +48,14 @@ export function SignIn() {
                         favoritos com seus amigos
                     </Text>
 
-                    <ButtonIcon
-                        title="Entrar com Discord"
-                        onPress={handleSignIn}
-                    />
+                    {
+                        loading ? <ActivityIndicator color={theme.colors.primary} /> :
+                            <ButtonIcon
+                                title="Entrar com Discord"
+                                onPress={handleSignIn}
+                            />
+                    }
+
                 </View>
             </View>
         </Background>
